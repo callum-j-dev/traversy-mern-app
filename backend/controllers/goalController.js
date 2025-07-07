740a8590540ada632358ad8e8c5d09ed048591ba
Callum Novak <callum.j.novak@gmail.com>
2025-07-07 13:57:31 -0400

    initial REST api for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,15 @@
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModel');
+
 // @desc    Get goals
 // @route   GET /apis/goals
 // @access Private
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message:'Get goals' });
+    const goals = await Goal.find();
+
+    res.status(200).json(goals);
 })
 
 // @desc    Set goal
@@ -20,8 +24,12 @@ const setGoal = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Please add a text field');
     } else {
+        const goal = await Goal.create({
+            text: req.body.text,
+        })
+
         console.log('Sending 200 response');
-        res.status(200).json({ message: 'Set goal' });
+        res.status(200).json(goal);
     }
 })
 // @desc    Update goal
@@ -29,7 +37,15 @@ const setGoal = asyncHandler(async (req, res) => {
 // @access  Private
 
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message:`Update goal ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.status(200).json(updatedGoal);
 })
 
 // @desc    Get goals
@@ -37,7 +53,16 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @access Private
 
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message:`Delete goal ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400);
+        throw new Error('Goal not found');
+    }
+
+    const deletedGoal = await Goal.findOneAndDelete(req.params.id)
+
+    res.status(200).json({message:`Deleted goal ${req.params.id}`});
 })
 
 module.exports = {
